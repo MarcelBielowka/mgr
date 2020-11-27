8943e9fb1af6d5d638dcd29555b28269e2bfff39
Wlochacz <marcelbielowka@gmail.com>
2020-11-28 00:01:53 +0100

    Further analysis of prices

--- PriceModellingAux.jl
+++ PriceModellingAux.jl
@@ -1,4 +1,5 @@
-using CSV, DataFrames, Plots, HypothesisTests, Dates
+using CSV, DataFrames, Plots, HypothesisTests, Dates,
+using StatsBase, LinearAlgebra, RCall
 
 dfPriceDataRaw = CSV.File("C:/Users/Marcel/Desktop/mgr/data/POLPX_DA_all.csv") |>
     DataFrame
@@ -9,7 +10,37 @@ rename!(dfPriceDataRaw, ["trade_date", "delivery_date", "delivery_hour", "price"
 
 dfPriceDataRaw["log_price"] = log.(dfPriceDataRaw["price"])
 
-a = dfPriceDataRaw[dfPriceDataRaw["delivery_hour"].=="9",:]
-a = a[(1766-100):1766,:]
-pvalue(HypothesisTests.ADFTest(a["log_price"], Symbol("none"), 8))
-plot(a["delivery_date"], a["log_price"])
+Subsample_price = dfPriceDataRaw[dfPriceDataRaw.delivery_date.<Dates.Date("2018-01-01", DateFormat("y-m-d")),:]
+Subsample_price = dfPriceDataRaw[dfPriceDataRaw.delivery_date.>=Dates.Date("2018-01-01", DateFormat("y-m-d")),:]
+Subsample_price = Subsample_price[Subsample_price.delivery_date.<Dates.Date("2020-01-01", DateFormat("y-m-d")),:]
+a = Subsample_price[Subsample_price["delivery_hour"].=="6",:]
+
+HypothesisTests.ADFTest(Subsample_price.price, Symbol("constant"), 500)
+HypothesisTests.ADFTest(Subsample_price.price, Symbol("trend"), 500)
+HypothesisTests.ADFTest(Subsample_price.price, Symbol("squared_trend"), 500)
+
+HypothesisTests.ADFTest(a["price"], Symbol("constant"), 40)
+HypothesisTests.ADFTest(a["price"], Symbol("trend"), 40)
+HypothesisTests.ADFTest(a["price"], Symbol("squared_trend"), 40)
+
+HypothesisTests.ADFTest(diff(a["price"]), Symbol("constant"), 40)
+HypothesisTests.ADFTest(diff(a["price"]), Symbol("trend"), 40)
+HypothesisTests.ADFTest(diff(a["price"]), Symbol("squared_trend"), 40)
+
+@rlibrary stats
+R"acf"(diff(a.log_price), lag = 28)
+R"acf"(a.log_price, lag = 50)
+R"pacf"(diff(a.log_price), lag = 40)
+R"pacf"(a.log_price, lag = 50)
+
+R"acf"(Subsample_price.log_price, lag = 337)
+R"pacf"(Subsample_price.log_price, lag = 336)
+
+PriceModel = R"arima"(a.log_price, order = R"c(28,1,0)")
+
+# plot(a.delivery_date[2:361], diff(a.log_price))
+plot(a.delivery_date, a.price)
+plot(a.delivery_date[2:end], diff(log.(a.price)))
+plot(Subsample_price.delivery_date, Subsample_price.price)
+
+Dates.Date("2018-01-01", DateFormat("y-m-d"))
