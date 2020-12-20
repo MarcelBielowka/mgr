d0ccc5cbfaa52d13ef673e15ac28ec4e260b3711
Wlochacz <marcelbielowka@gmail.com>
2020-12-20 15:29:28 +0100

    Add some initial attempts to model prices

--- PriceModellingAux.jl
+++ PriceModellingAux.jl
@@ -1,5 +1,5 @@
-using CSV, DataFrames, Plots, HypothesisTests, Dates,
-using StatsBase, LinearAlgebra, RCall
+using CSV, DataFrames, Plots, HypothesisTests, Dates
+using StatsBase, LinearAlgebra, RCall, Random
 
 dfPriceDataRaw = CSV.File("C:/Users/Marcel/Desktop/mgr/data/POLPX_DA_all.csv") |>
     DataFrame
@@ -10,14 +10,13 @@ rename!(dfPriceDataRaw, ["trade_date", "delivery_date", "delivery_hour", "price"
 
 dfPriceDataRaw["log_price"] = log.(dfPriceDataRaw["price"])
 
-Subsample_price = dfPriceDataRaw[dfPriceDataRaw.delivery_date.<Dates.Date("2018-01-01", DateFormat("y-m-d")),:]
-Subsample_price = dfPriceDataRaw[dfPriceDataRaw.delivery_date.>=Dates.Date("2018-01-01", DateFormat("y-m-d")),:]
-Subsample_price = Subsample_price[Subsample_price.delivery_date.<Dates.Date("2020-01-01", DateFormat("y-m-d")),:]
-a = Subsample_price[Subsample_price["delivery_hour"].=="6",:]
-
-HypothesisTests.ADFTest(Subsample_price.price, Symbol("constant"), 500)
-HypothesisTests.ADFTest(Subsample_price.price, Symbol("trend"), 500)
-HypothesisTests.ADFTest(Subsample_price.price, Symbol("squared_trend"), 500)
+# Subsample_price = dfPriceDataRaw[dfPriceDataRaw.delivery_date.<Dates.Date("2018-01-01", DateFormat("y-m-d")),:]
+Subsample_price = dfPriceDataRaw[dfPriceDataRaw.delivery_date.>=Dates.Date("2016-01-01", DateFormat("y-m-d")),:]
+Subsample_price = Subsample_price[Subsample_price.delivery_date.<Dates.Date("2018-01-01", DateFormat("y-m-d")),:]
+a = Subsample_price[Subsample_price["delivery_hour"].=="3",:]
+b = dfPriceDataRaw[dfPriceDataRaw.delivery_hour.=="3",:]
+plot(a.delivery_date, a.price)
+plot(b.delivery_date, b.price)
 
 HypothesisTests.ADFTest(a["price"], Symbol("constant"), 40)
 HypothesisTests.ADFTest(a["price"], Symbol("trend"), 40)
@@ -28,15 +27,18 @@ HypothesisTests.ADFTest(diff(a["price"]), Symbol("trend"), 40)
 HypothesisTests.ADFTest(diff(a["price"]), Symbol("squared_trend"), 40)
 
 @rlibrary stats
-R"acf"(diff(a.log_price), lag = 28)
-R"acf"(a.log_price, lag = 50)
-R"pacf"(diff(a.log_price), lag = 40)
-R"pacf"(a.log_price, lag = 50)
+R"acf"(diff(a.log_price), lag = 50)
+# R"acf"(a.log_price, lag = 50)
+R"pacf"(diff(a.log_price), lag = 60)
+# R"pacf"(a.log_price, lag = 50)
 
-R"acf"(Subsample_price.log_price, lag = 337)
-R"pacf"(Subsample_price.log_price, lag = 336)
+Random.seed!(72945)
 
-PriceModel = R"arima"(a.log_price, order = R"c(28,1,0)")
+PriceModelInit = R"arima"(a.log_price, order = R"c(28,1,0)",
+    fixed = R"c(NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,0,NA,NA)")
+
+PriceModel = R"arima"(a.log_price, order = R"c(28,1,0)",
+    fixed = R"c(NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,0,NA,0,0,0,0,NA,NA)")
 
 # plot(a.delivery_date[2:361], diff(a.log_price))
 plot(a.delivery_date, a.price)
