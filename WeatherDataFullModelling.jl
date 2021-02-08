ab2f33315fe3c39a914331ea8907b1f742841359
Wlochacz <>
2021-02-08 23:16:04 +0100

    Remodel the weather code

--- WeatherDataFullModelling.jl
+++ WeatherDataFullModelling.jl
@@ -3,58 +3,88 @@ using Dates, RCall, StatsPlots, Distributions, Pipe
 
 include("WeatherDataFullPreparation.jl")
 
-dfWeatherData = ReadData()
-dfWeatherData.Temperature = dfWeatherData.Temperature .+ 273.15
-dfWeatherData.MonthPart =
-    Dates.day.(dfWeatherData.date_nohour) .> Dates.daysinmonth.(dfWeatherData.date_nohour)/2
+# set seed
+Random.seed!(72945)
 
-dfWeatherDataGrouped = groupby(dfWeatherData, [:month, :MonthPart])
+# extract data
+dfWeatherDataUngrouped = ReadData()
+#group data
+WeatherDistParameters, dfWeatherDataGropued = RetrieveGroupedData(dfWeatherDataUngrouped)
+WeatherDistParameters
+dfWeatherDataGropued
 
-GroupsMapping = sort(dfWeatherDataGrouped.keymap, by = values)
-GroupsMapping.vals[1]
-GroupsMapping.keys
-
-WeatherDistParameters = Dict{}()
-month, monthperiod = GroupsMapping.keys[1]
-
-m = dfWeatherDataGrouped[1]
-m_subs = m[m.hour .== 14, :]
-a = fitdistr(m_subs.WindSpeed[m_subs.WindSpeed.>0], "weibull", lower = "c(0,0)")
-
-for PeriodNum in 1:24
-    CurrentPeriod = GroupsMapping.vals[PeriodNum]
-    dfCurrentPeriod = dfWeatherDataGrouped[CurrentPeriod]
-    month, MonthPeriod = GroupsMapping.keys[PeriodNum]
-
-    for hour in extrema(dfWeatherDataGrouped[1].hour)[1]:extrema(dfWeatherDataGrouped[1].hour)[2]
-        dfCurrentHour = filter(row -> row.hour .== hour, dfCurrentPeriod)
-        println("Current month: $month, period: $PeriodNum , hour: $hour")
-
-        DistWindMASS = fitdistr(dfCurrentHour.WindSpeed[dfCurrentHour.WindSpeed.>0], "weibull", lower = R"c(0,0)")
-        DistSolarMASS = fitdistr(dfCurrentHour.Irradiation, "normal")
-        DistTempMASS = fitdistr(dfCurrentHour.Temperature, "normal")
-
-        push!(WeatherDistParameters, (month, MonthPeriod, hour) => ["WindParam" => [DistWindMASS[1][1] DistWindMASS[1][2]],
-                                                                      "SolarParam" => [DistSolarMASS[1][1] DistSolarMASS[1][2]],
-                                                                      "TempParam" => [DistTempMASS[1][1] DistTempMASS[1][2]],
-                                                               ])
-    end
-end
-
-temp = dfWeatherDataGrouped[22][dfWeatherDataGrouped[22].hour .== 18,:].WindSpeed
-fitdistr(temp[temp.>0], "weibull", lower = R"c(0,0)")
-
-
+# select the day for which the analysis is conducted
 DayOfYear = rand(1:365)
-
 DateAnalysed = Date(2018,12,31) + Dates.Day(DayOfYear)
 MonthPart = Dates.day.(DateAnalysed) .> Dates.daysinmonth.(DateAnalysed)/2
 
-for hour in 0:23
-    Weather = WeatherDistParameters[(Dates.month(DateAnalysed), MonthPart, hour)]
-    println("$hour")
-    println(Weather)
-end
+# take the parameters for that day
+WeatherParamsDay = [WeatherDistParameters[(Dates.month(DateAnalysed), MonthPart, hour)] for hour in 0:23]
+WeatherParamsDay[1][1][2]
+# prepare the relevant distributions
+WindDistHour = Weibull(WeatherParamsDay[1][1][2][1], WeatherParamsDay[1][1][2][2])
+
+# calculate the forecasts
+WindForecastDist = [mean(rand(WindDistHour, 1000)) for i in 1:10000]
+ProductionForecastWind = WindProductionForecast.(2.0, WindForecastDist, 11.5, 3.0, 20.0)
+mean(ProductionForecastWind)
+ProductionForecastWindPoint = WindProductionForecast(2.0, mean(WindForecastDist), 11.5, 3.0, 20.0)
+
+histogram(WindForecastDist, normalize = true)
+histogram(ProductionForecastWind, normalize = true)
+
+#dfWeatherData.Temperature = dfWeatherData.Temperature .+ 273.15
+#dfWeatherData.MonthPart =
+#    Dates.day.(dfWeatherData.date_nohour) .> Dates.daysinmonth.(dfWeatherData.date_nohour)/2
+
+#dfWeatherDataGrouped = groupby(dfWeatherData, [:month, :MonthPart])
+
+#GroupsMapping = sort(dfWeatherDataGrouped.keymap, by = values)
+#GroupsMapping.vals[1]
+#GroupsMapping.keys
+
+#WeatherDistParameters = Dict{}()
+#month, monthperiod = GroupsMapping.keys[1]
+
+#m = dfWeatherDataGrouped[1]
+#m_subs = m[m.hour .== 14, :]
+#a = fitdistr(m_subs.WindSpeed[m_subs.WindSpeed.>0], "weibull", lower = "c(0,0)")
+
+#for PeriodNum in 1:24
+#    CurrentPeriod = GroupsMapping.vals[PeriodNum]
+#    dfCurrentPeriod = dfWeatherDataGrouped[CurrentPeriod]
+#    month, MonthPeriod = GroupsMapping.keys[PeriodNum]
+#
+#    for hour in extrema(dfWeatherDataGrouped[1].hour)[1]:extrema(dfWeatherDataGrouped[1].hour)[2]
+#        dfCurrentHour = filter(row -> row.hour .== hour, dfCurrentPeriod)
+#        println("Current month: $month, period: $PeriodNum , hour: $hour")
+
+#        DistWindMASS = fitdistr(dfCurrentHour.WindSpeed[dfCurrentHour.WindSpeed.>0], "weibull", lower = R"c(0,0)")
+#        DistSolarMASS = fitdistr(dfCurrentHour.Irradiation, "normal")
+#        DistTempMASS = fitdistr(dfCurrentHour.Temperature, "normal")
+
+#        push!(WeatherDistParameters, (month, MonthPeriod, hour) => ["WindParam" => [DistWindMASS[1][1] DistWindMASS[1][2]],
+#                                                                      "SolarParam" => [DistSolarMASS[1][1] DistSolarMASS[1][2]],
+#                                                                      "TempParam" => [DistTempMASS[1][1] DistTempMASS[1][2]],
+#                                                               ])
+#    end
+#end
+
+#temp = dfWeatherDataGrouped[22][dfWeatherDataGrouped[22].hour .== 18,:].WindSpeed
+#fitdistr(temp[temp.>0], "weibull", lower = R"c(0,0)")
+
+
+
+#DayOfYear = rand(1:365)
+
+#DateAnalysed = Date(2018,12,31) + Dates.Day(DayOfYear)
+#MonthPart = Dates.day.(DateAnalysed) .> Dates.daysinmonth.(DateAnalysed)/2
+
+# for hour in 0:23
+#    Weather = WeatherDistParameters[(Dates.month(DateAnalysed), MonthPart, hour)]
+#    println("$hour")
+#    println(Weather)
+#end
 
 a = WeatherDistParameters[2, false, 1][1][2]
 DistTemp = Weibull(WeatherDistParameters[1, true, 22][1][2][1], WeatherDistParameters[1, true, 22][1][2][2])
@@ -66,19 +96,6 @@ t = [mean(rand(DistTemp, 1000)) for i in 1:10000]
 mean(t)
 histogram(t, normalize = true)
 plot!(Normal(mean(t), std(t)), lw = 3)
-
-function WindProductionForecast(P_nam, V, V_nam, V_cutin, V_cutoff)
-    if V < V_cutin
-        P_output = 0
-    elseif V >= V_cutin && V < V_nam
-        P_output = ((V - V_cutin) ^ 3) / -((V_cutin - V_nam)^3) * P_nam
-    elseif V >= V_nam && V < V_cutoff
-        P_output = P_nam
-    else
-        P_output = 0
-    end
-    return P_output
-end
 
 ForecastWind = WindProductionForecast.(2.0, mean(t), 11.5, 3.0, 20.0)*10
 mean(ForecastWind) * 10
--- WeatherDataFullPreparation.jl
+++ WeatherDataFullPreparation.jl
@@ -50,27 +50,96 @@ function ReadData()
     return dfWeatherDataNoMissing
 end
 
+## Grouping data for modelling purposes
+
+function RetrieveGroupedData(dfWeatherData; kelvins::Bool = true)
+    if kelvins
+        dfWeatherData.Temperature = dfWeatherData.Temperature .+ 273.15
+    end
+    dfWeatherData.MonthPart =
+        Dates.day.(dfWeatherData.date_nohour) .> Dates.daysinmonth.(dfWeatherData.date_nohour)/2
+
+    dfWeatherDataGrouped = groupby(dfWeatherData, [:month, :MonthPart])
+
+    GroupsMapping = sort(dfWeatherDataGrouped.keymap, by = values)
+
+    WeatherDistParameters = Dict{}()
+
+    for PeriodNum in 1:24
+        CurrentPeriod = GroupsMapping.vals[PeriodNum]
+        dfCurrentPeriod = dfWeatherDataGrouped[CurrentPeriod]
+        month, MonthPeriod = GroupsMapping.keys[PeriodNum]
+
+        for hour in extrema(dfWeatherDataGrouped[1].hour)[1]:extrema(dfWeatherDataGrouped[1].hour)[2]
+            dfCurrentHour = filter(row -> row.hour .== hour, dfCurrentPeriod)
+            println("Current month: $month, period: $PeriodNum , hour: $hour")
+
+            DistWindMASS = fitdistr(dfCurrentHour.WindSpeed[dfCurrentHour.WindSpeed.>0], "weibull", lower = R"c(0,0)")
+            DistSolarMASS = fitdistr(dfCurrentHour.Irradiation, "normal")
+            DistTempMASS = fitdistr(dfCurrentHour.Temperature, "normal")
+
+            push!(WeatherDistParameters, (month, MonthPeriod, hour) => ["WindParam" => [DistWindMASS[1][1] DistWindMASS[1][2]],
+                                                                          "SolarParam" => [DistSolarMASS[1][1] DistSolarMASS[1][2]],
+                                                                          "TempParam" => [DistTempMASS[1][1] DistTempMASS[1][2]],
+                                                                   ])
+        end
+    end
+
+    return WeatherDistParameters, dfWeatherDataGrouped
+end
+
+##
+# wind production
+function WindProductionForecast(P_nam, V, V_nam, V_cutin, V_cutoff)
+    if V < V_cutin
+        P_output = 0
+    elseif V >= V_cutin && V < V_nam
+        P_output = ((V - V_cutin) ^ 3) / -((V_cutin - V_nam)^3) * P_nam
+    elseif V >= V_nam && V < V_cutoff
+        P_output = P_nam
+    else
+        P_output = 0
+    end
+    return P_output
+end
+
+
+##
+# solar production
+function SolarCellTemp(TempAmb, Noct, Irradiation; TempConst = 20, IrrConst = 800)
+    C = (Noct - TempConst)/IrrConst
+    SolarTemp = TempAmb + C * Irradiation
+    return SolarTemp
+end
+
+function SolarProductionForecast(P_STC, Irradiation, TempAmb, γ_temp, Noct; Irr_STC = 1000, T_STC = 25)
+    TempCell = SolarCellTemp(TempAmb = TempAmb, Noct = Noct,
+        Irradiation = Irradiation)
+    P_output = P_STC * Irradiation / Irr_STC * (1 - γ_temp * (TempCell - T_STC))
+    return P_output
+end
+
 ## plotting
 #
-function Plotting()
-    plot1 = @df dfWeatherDataNoMissing StatsPlots.plot(:date, :WindSpeed,
+function Plotting(dfData, dfDataWithoutMissing)
+    plot1 = @df dfDataWithoutMissing StatsPlots.plot(:date, :WindSpeed,
         label = "Wind Speed", legend = :none, lw = 2,
         color = RGB(192/255,0,0), linealpha = 0.6,
         title = "Hourly wind speed")
 
-    plot2 = @df dfWeatherDataNoMissing StatsPlots.plot(:date, :Irradiation,
+    plot2 = @df dfDataWithoutMissing StatsPlots.plot(:date, :Irradiation,
             label = "Wind Speed", legend = :none, lw = 2,
             color = RGB(192/255,192/255,192/255), linealpha = 0.6,
             title = "Hourly irradiation")
 
-    plot3 = @df dfWeatherDataNoMissing StatsPlots.plot(:date, :Temperature,
+    plot3 = @df dfDataWithoutMissing StatsPlots.plot(:date, :Temperature,
             label = "Wind Speed", legend = :none, lw = 2,
             color = RGB(100/255,100/255,100/255), linealpha = 0.6,
             title = "Hourly temperature")
 
 
     ## plotting monthly averages
-    dfWeatherDataGrouped = @pipe groupby(dfWeatherData, [:month, :year]) |>
+    dfWeatherDataGrouped = @pipe groupby(dfData, [:month, :year]) |>
         combine(_, [:WindSpeed => mean => :WindSpeedMonthAvg,
                 :Irradiation => mean => :IrradiationMonthAvg,
                 :Temperature => mean => :TemperatureMonthAvg,
@@ -84,43 +153,41 @@ function Plotting()
     dropmissing!(dfWeatherDataGrouped)
     dfWeatherDataGrouped
 
-    plot1 = @df dfWeatherDataGrouped StatsPlots.plot(:Date, :WindSpeedMonthAvg,
+    plot4 = @df dfWeatherDataGrouped StatsPlots.plot(:Date, :WindSpeedMonthAvg,
         label = "Wind Speed", legend = :none, lw = 2, xticks = :none,
         color = RGB(192/255,0,0),
         title = "Monthly average of wind speed")
-    plot2 = @df dfWeatherDataGrouped StatsPlots.plot(:Date, :TemperatureMonthAvg,
+    plot5 = @df dfWeatherDataGrouped StatsPlots.plot(:Date, :TemperatureMonthAvg,
         label = "Temperature", legend = :none, lw = 2,
         color = RGB(100/255,100/255,100/255), xticks = :none,
         title = "Monthly average of temperature")
-    plot3 = @df dfWeatherDataGrouped StatsPlots.plot(:Date, :IrradiationMonthAvg,
+    plot6 = @df dfWeatherDataGrouped StatsPlots.plot(:Date, :IrradiationMonthAvg,
         label = "Irradiation", legend = :none, lw = 2,
         color = RGB(192/255,192/255,192/255), xticks = :none,
         title = "Monthly average of Irradiation")
-    plot4 = plot()
 
     # plot(plot1,plot2,plot3,plot4, layout = (2,2))
-    plot(plot1,plot2,plot3, layout = (3,1))
+    FinalPlot4 = plot(plot4,plot5,plot6, layout = (3,1))
 
-    plot5 = @df dfWeatherDataGrouped StatsPlots.plot(:Date, :WindSpeedMonthStd,
+    plot7 = @df dfWeatherDataGrouped StatsPlots.plot(:Date, :WindSpeedMonthStd,
         label = "Wind Speed", legend = :none, lw = 2, xticks = :none,
         color = RGB(192/255,0,0),
         title = "Monthly std of wind speed")
-    plot6 = @df dfWeatherDataGrouped StatsPlots.plot(:Date, :TemperatureMonthStd,
+    plot8 = @df dfWeatherDataGrouped StatsPlots.plot(:Date, :TemperatureMonthStd,
         label = "Temperature", legend = :none, lw = 2,
         color = RGB(100/255,100/255,100/255), xticks = :none,
         title = "Monthly std of temperature")
-    plot7 = @df dfWeatherDataGrouped StatsPlots.plot(:Date, :IrradiationMonthStd,
+    plot9 = @df dfWeatherDataGrouped StatsPlots.plot(:Date, :IrradiationMonthStd,
         label = "Irradiation", legend = :none, lw = 2,
         color = RGB(192/255,192/255,192/255), xticks = :none,
         title = "Monthly std of Irradiation")
-    plot8 = plot()
 
     # plot(plot1,plot2,plot3,plot4, layout = (2,2))
-    plot(plot5,plot6,plot7, layout = (3,1))
+    FinalPlot5 = plot(plot5,plot6,plot7, layout = (3,1))
 
     MonthsAsStrings = ["Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec"]
 
-    plot1_1 = @df dfWeatherDataGrouped StatsPlots.groupedbar(:month, :IrradiationMonthAvg,
+    plot10 = @df dfWeatherDataGrouped StatsPlots.groupedbar(:month, :IrradiationMonthAvg,
                 group = :year,
                 bar_position = :dodge,
                 color = [RGB(10/255,0,0) RGB(40/255,0,0) RGB(70/255,0,0) RGB(100/255,0,0) RGB(130/255,0,0) RGB(160/255,0,0) RGB(192/255,0,0) RGB(220/255,0,0) RGB(255/255,0,0)],
@@ -129,7 +196,7 @@ function Plotting()
                 ylabel = "Average monthly irradiation",
                 xticks = (1:12, MonthsAsStrings))
 
-    plot1_2 = @df dfWeatherDataGrouped StatsPlots.groupedbar(:month, :TemperatureMonthAvg,
+    plot11 = @df dfWeatherDataGrouped StatsPlots.groupedbar(:month, :TemperatureMonthAvg,
                 group = :year,
                 bar_position = :dodge,
                 color = [RGB(10/255,0,0) RGB(40/255,0,0) RGB(70/255,0,0) RGB(100/255,0,0) RGB(130/255,0,0) RGB(160/255,0,0) RGB(192/255,0,0) RGB(220/255,0,0) RGB(255/255,0,0)],
@@ -139,7 +206,7 @@ function Plotting()
                 ylabel = "Average monthly temperature",
                 xticks = (1:12, MonthsAsStrings))
 
-    plot1_3 = @df dfWeatherDataGrouped StatsPlots.groupedbar(:month, :WindSpeedMonthAvg,
+    plot12 = @df dfWeatherDataGrouped StatsPlots.groupedbar(:month, :WindSpeedMonthAvg,
                 group = :year,
                 bar_position = :dodge,
                 color = [RGB(10/255,0,0) RGB(40/255,0,0) RGB(70/255,0,0) RGB(100/255,0,0) RGB(130/255,0,0) RGB(160/255,0,0) RGB(192/255,0,0) RGB(220/255,0,0) RGB(255/255,0,0)],
@@ -148,5 +215,6 @@ function Plotting()
                 ylabel = "Average monthly wind speed",
                 xticks = (1:12, MonthsAsStrings))
 
+    return plot1, plot2, plot3, FinalPlot4, FinalPlot5, plot10, plot11, plot12
 end
     # jeden kolor dla barow + srednia wieloletnia dla danego miesiaca -
