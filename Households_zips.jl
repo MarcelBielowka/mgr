5c60cb85d2c1bfe199554b5bb64e961f219d6516
Wlochacz <>
2021-03-22 22:56:53 +0100

    Latest version

--- Households_zips.jl
+++ Households_zips.jl
@@ -12,8 +12,9 @@ myDataShort = filter(row -> (row.Date > Dates.Date("2012-12-31") && row.Date < D
 
 myDataComplete = ClearAndModifyHouseholdData(myDataShort)
 
-myDataCompleteEnd = PrepareDataForClustering(myDataComplete)
+myDataCompleteEnd = PrepareDataForClustering(myDataComplete[1])
 myDataComplete = nothing
+myData = nothing
 testData = myDataCompleteEnd[1]
 
 CurrentPeriod = unstack(testData, :IDAndDay, :Consumption)
@@ -23,3 +24,8 @@ for column in eachcol(CurrentPeriod)
     Impute.impute!(column, Impute.NOCB())
 end
 disallowmissing!(CurrentPeriod)
+TestClusters = Clustering.kmeans(
+    Matrix(CurrentPeriod[:,4:size(CurrentPeriod)[2]]), 2)
+
+size(CurrentPeriod, 2) - 4
+TestClusters.counts
