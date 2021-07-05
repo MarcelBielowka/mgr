36aa565ecb7c4ccb4c30ff4de1599630aa4ce688
Wlochacz <marcelbielowka@gmail.com>
2021-07-05 02:17:18 +0200

    Propagate changes to main as well

--- main.jl
+++ main.jl
@@ -72,7 +72,9 @@ MyWindPark = GetWindPark(2000.0, 11.5, 3.0, 20.0, Weather, 1)
 ####
 # Initiate the households
 ####
-Households = Get_⌂(cHouseholdsDir, dUKHolidayCalendar, 100, 11.7, 7.0, 5.0, 10)
+Households = Get_⌂(cHouseholdsDir, dUKHolidayCalendar, dPLHolidayCalendar,
+    cWeatherPricesDataWindowStart, cWeatherPricesDataWindowEnd,
+    100, 11.7, 7.0, 5.0, 10)
 #Households.EnergyConsumption[(12,6)]
 
 ####
