fe59f3e06b16150bac8a7611535cdc0a61eb4bf8
Wlochacz <marcelbielowka@gmail.com>
2021-07-13 21:49:48 +0200

    last commit before major overhaul

--- DefineRL.jl
+++ DefineRL.jl
@@ -138,9 +138,9 @@ function CalculateReward(Microgrid::Microgrid, Action::Float64, ActualAction::Fl
     else
         iReward = iGridVolume * dictRewards["iPriceBuy"]
     end
-    #if abs(Action / ActualAction) > 1.3
-    #    iReward = iReward - min(100000 * abs(Action / ActualAction), 1e7)
-    #end
+    if abs(Action / ActualAction) > 1.3
+        iReward = iReward - min(100000 * abs(Action / ActualAction), 1e7)
+    end
     return iReward
 end
 
