732e74fe4abbd5e55c21fc2ab83ebaa1dacc08b9
Wlochacz <marcelbielowka@gmail.com>
2021-10-16 17:01:42 +0200

    Fix sigma

--- DefineRL.jl
+++ DefineRL.jl
@@ -62,11 +62,12 @@ end
 function ActorLoss(x, Actions, A; ι::Float64 = 0.001)
     #println("μ_policy: $μ_policy")
     #println(typeof(μ_policy))
-    PolicyParameters = MyMicrogrid.Brain.policy_net(x)
-    μ_hat = PolicyParameters[1,:]
-    σ_hat = deepcopy(PolicyParameters[2,:])
-    σ_hat = softplus.(σ_hat) .+ 1e-3
-    # μ_hat = MyMicrogrid.Brain.policy_net(x)
+    #PolicyParameters = MyMicrogrid.Brain.policy_net(x)
+    #μ_hat = PolicyParameters[1,:]
+    #σ_hat = deepcopy(PolicyParameters[2,:])
+    #σ_hat = softplus.(σ_hat) .+ 1e-3
+    μ_hat = MyMicrogrid.Brain.policy_net(x)
+    σ_hat = 0.01
     #MyMicrogrid.Brain.cPolicyOutputLayerType == "sigmoid" ? σ_hat = 0.01 : σ_hat = 1.0
     Policy = Distributions.Normal.(μ_hat, σ_hat)
     #println("Policy: $Policy")
@@ -199,12 +200,12 @@ end
 function Forward(Microgrid::Microgrid, state::Vector, bσFixed::Bool, dictNormParams::Dict, iLookBack::Int)
     # StateForLearning = deepcopy(Microgrid.State)
     StateForLearning = @pipe deepcopy(Microgrid.State) |> NormaliseState!(_, dictNormParams, iLookBack)
-    # μ_hat = Microgrid.Brain.policy_net(StateForLearning)[1]    # wektor p-w na bazie sieci aktora
-    # MyMicrogrid.Brain.cPolicyOutputLayerType == "sigmoid" ? σ_hat  = 0.01 : σ_hat  = 1.0
-    PolicyParameters = MyMicrogrid.Brain.policy_net(StateForLearning)
-    μ_hat = PolicyParameters[1,:]
-    σ_hat = deepcopy(PolicyParameters[2,:])
-    σ_hat = softplus.(σ_hat) .+ 1e-3
+    μ_hat = Microgrid.Brain.policy_net(StateForLearning)    # wektor p-w na bazie sieci aktora
+    σ_hat = 0.01
+    # PolicyParameters = MyMicrogrid.Brain.policy_net(StateForLearning)
+    # μ_hat = PolicyParameters[1,:]
+    # σ_hat = deepcopy(PolicyParameters[2,:])
+    # σ_hat = softplus.(σ_hat) .+ 1e-3
     println("Policy params: $μ_hat, $σ_hat")
     Policy = Distributions.Normal.(μ_hat, σ_hat)
     #MyMicrogrid.Brain.cPolicyOutputLayerType == "sigmoid" ? iσFixed = 0.01 : iσFixed = 1.0
