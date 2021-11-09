c237fec733ca820aad1a0eed6c30d412a067dd17
Wlochacz <marcelbielowka@gmail.com>
2021-11-10 00:25:33 +0100

    Add some stuff

--- DefineRL.jl
+++ DefineRL.jl
@@ -141,7 +141,7 @@ function ChargeOrDischargeBattery!(Microgrid::Microgrid, Action::Float64, iLookB
     #else
     #    iChargeDischargeVolume = deepcopy(Action)
     #end
-    iChargeDischargeVolume = deepcopy(Action) * Microgrid.EnergyStorage.iMaxCapacity
+    iChargeDischargeVolume = deepcopy(Action)
     # iChargeDischargeVolume = deepcopy(Action)
     if iChargeDischargeVolume >= 0
         iMaxPossibleCharge = min(Microgrid.EnergyStorage.iChargeRate,
@@ -154,8 +154,8 @@ function ChargeOrDischargeBattery!(Microgrid::Microgrid, Action::Float64, iLookB
         #else
         #    ActualAction = iCharge
         #end
-        ActualAction = iChargeNormalised
-        # ActualAction = iCharge
+        # ActualAction = iChargeNormalised
+        ActualAction = iCharge
         if bLog
             println("Actual charge of battery: ", round(iCharge; digits = 2))
         end
@@ -170,8 +170,8 @@ function ChargeOrDischargeBattery!(Microgrid::Microgrid, Action::Float64, iLookB
         #else
         #    ActualAction = iDischarge
         #end
-        ActualAction = iDischargeNormalised
-        # ActualAction = iDischarge
+        # ActualAction = iDischargeNormalised
+        ActualAction = iDischarge
         if bLog
             println("Actual discharge of battery: ", round(iDischarge; digits = 2))
         end
@@ -189,8 +189,8 @@ function CalculateReward(Microgrid::Microgrid, State::Vector, iLookBack::Int,
     # iMicrogridVolume = deepcopy(ActualAction) * State[1]
     # iMicrogridVolume = deepcopy(ActualAction)
     # iGridVolume = State[1] - iMicrogridVolume
-    iGridVolume = State[1] - ActualAction * Microgrid.EnergyStorage.iMaxCapacity
-    iMicrogridVolume = State[1] - iGridVolume
+    iGridVolume = State[1] - ActualAction
+    iMicrogridVolume = ActualAction
     #iGridPrice = Microgrid.DayAheadPricesHandler.dfQuantilesOfPrices.iMedian[1]
     #iReward = iGridVolume * iGridPrice
     #dictRewards = GetReward(Microgrid, iTimeStep)
@@ -256,7 +256,8 @@ function Act!(Microgrid::Microgrid, iTimeStep::Int, iHorizon::Int, iLookBack::In
     if bLog
         # println("Time step $iTimeStep, intended action $Action kW, prod-cons mismatch ", CurrentState[iLookBack+1])
         println("Currently free storage capacity: ", Microgrid.State[length(Microgrid.State)] * Microgrid.EnergyStorage.iMaxCapacity)
-        println("Intended action $ActionForPrint % of storage capacity")
+        # println("Intended action $ActionForPrint % of storage capacity")
+        println("Intended action $Action kWh")
         println("Current prod-cons mismatch ", round(CurrentState[1]; digits = 2))
         #if Microgrid.Brain.cPolicyOutputLayerType == "sigmoid"
         #    println("Time step $iTimeStep, intended action $ActionForPrint % of mismatch, prod-cons mismatch ", CurrentState[iLookBack+1])
