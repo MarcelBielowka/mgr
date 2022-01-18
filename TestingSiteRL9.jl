ff632fd8ecf5eec8b543b9a0b1497efb0e750383
Wlochacz <marcelbielowka@gmail.com>
2022-01-18 22:11:19 +0100

    Further stuff

--- TestingSiteRL9.jl
+++ TestingSiteRL9.jl
@@ -136,12 +136,19 @@ BetaParamsAugStacked = stack(BetaParamsAug, [:iInitialTestResult, :iResultAfterT
 BetaParamsAugStackedTraining = stack(BetaParamsAug, [:iTrainResult], :iβ)
 rename!(BetaParamsAugStackedTraining, [:iβ, :cName, :iTrainResult])
 
-@df BetaParamsAugStackedTraining StatsPlots.plot(
-    repeat(collect(1:1:100), 4),
+PlotBetaTuning = @df BetaParamsAugStackedTraining StatsPlots.plot(
+    repeat(collect(1:1:40), 4),
     :iTrainResult,
     group = :iβ,
     legend = :topleft,
+    legendtitle = "β",
+    legendtitlefontsize = 10,
+    legendfontsize = 7,
+    xlabel = "Number of epsiodes",
+    ylabel = "Cumulated reward",
     color = [RGB(192/255, 0, 0) RGB(192/255, 192/255, 0) RGB(192/255, 192/255, 192/255) RGB(0, 0, 0)])
+
+savefig(PlotBetaTuning, "C:/Users/Marcel/Desktop/mgr/graphs/BetaTuning.png")
 
 #@df BetaParamsAug groupedboxplot(string.(:iβ), :iResultAfterTraining)
 #@df BetaParamsAug groupedboxplot!(string.(:iβ), :iInitialTestResult)
@@ -149,9 +156,17 @@ rename!(BetaParamsAugStackedTraining, [:iβ, :cName, :iTrainResult])
 #@df BetaParamsAug violin(string.(:iβ), :iResultAfterTraining, side = :left)
 #@df BetaParamsAug violin!(string.(:iβ), :iInitialTestResult, side = :right)
 
-@df BetaParamsAugStacked groupedboxplot(string.(:iβ), :value, group = :variable,
-    label = ["Result before training" "Result after training"], legend = :right,
-    color = [RGB(192/255, 192/255, 192/255) RGB(192/255, 0, 0)])
+PlotBetaTuningAvg = @df BetaParamsAugStacked groupedboxplot(
+    string.(:iβ),
+    :value,
+    group = :variable,
+    label = ["Result before training" "Result after training"],
+    legend = :right,
+    color = [RGB(192/255, 192/255, 192/255) RGB(192/255, 0, 0)],
+    xlabel = "β",
+    ylabel = "Cumulated reward, testing period"
+    )
+savefig(PlotBetaTuningAvg, "C:/Users/Marcel/Desktop/mgr/graphs/BetaTuningAvg.png")
 
 # plots commercial params
 CommercialParamsAugTransformed = @pipe CommercialParamsAug |>
@@ -163,19 +178,19 @@ insertcols!(CommercialParamsAugTransformed,
     (CommercialParamsAugTransformed.iTrainResult .- CommercialParamsAugTransformed.iTrainResultMin) ./
         (CommercialParamsAugTransformed.iTrainResultMax .- CommercialParamsAugTransformed.iTrainResultMin)
     )
-@df CommercialParamsAugTransformed StatsPlots.plot(
-    repeat(collect(1:1:50), 20),
+PlotCommercialParams = @df CommercialParamsAugTransformed StatsPlots.plot(
+    repeat(collect(1:1:40), 20),
     string.(:iGridLongVolumeCoefficient),
     :iTrainResultNormalised,
-    line_z = (:iLookBack),
+    line_z = Int.(:iLookBack),
     group = (:iGridLongVolumeCoefficient, :iLookBack),
     camera = (75, 30),
     # vlinecolor = :iLookBack,
     # linecolor = [RGB(30/255, 0, 0) RGB(100/255, 0, 0) RGB(192/255, 0, 0) RGB(30/255, 30/255, 30/255) RGB(100/255, 100/255, 100/255) RGB(140/255, 140/255, 140/255)],
-    linecolor = :OrRd_8,
+    linecolor = :sun,
     # legend = :none,
     label = "",
-    lw = 2,
+    linewidth = 3,
     linealpha = 0.7,
     xlabel = "",
     xrotation = -45,
@@ -184,8 +199,15 @@ insertcols!(CommercialParamsAugTransformed,
     # xguidefontsize = 18,
     ylabel = "κ coefficient" ,
     zlabel = "Reward in the episode, normalised",
-    size = (800, 600)
+    size = (800, 600),
+    colorbar_title = "\n\n\n Look ahead horizon",
+    colorbar_ticks = [0 2 4 6 12 24],
+    left_margin = 2Plots.mm,
+    right_margin = 2Plots.mm
+    # levels = [i for i in 0:24]
 )
+
+savefig(PlotCommercialParams, "C:/Users/Marcel/Desktop/mgr/graphs/CommercialParamsTuning.png")
 
 # plots NN
 NNParamsAugTransformed = @pipe NNParamsAug |>
